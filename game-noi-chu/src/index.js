0f67e6df2f8dec0fb1b199b7ca0d365efaf73933
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2022-04-10 23:19:10 +0700

    add bootstrap

--- game-noi-chu/src/index.js
+++ game-noi-chu/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
