b7feeb9826b6a723f9a3125a4335b894c8fac8f3
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2022-04-11 20:28:08 +0700

    implement the UI for start page

--- game-noi-chu/src/Components/StartPage/startPage.js
+++ game-noi-chu/src/Components/StartPage/startPage.js
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+// css
+import "../../css/startPage.css";
+
 export const StartPage = (props) => {
+
+    // React Router V6
+    let navigate = useNavigate();
+
+    const onStartGameClick = () => {
+        let path = 'not-found';
+        navigate(path);
+    }
+
     return (
-        <div>Start Page</div>
+        <div className="d-flex justify-content-center">
+            <button
+                type="button"
+                className="start-button mt-5"
+                onClick={onStartGameClick}
+            >
+                Get Started
+            </button>
+        </div>
     );
 }
