335345a72514ece7d4b7f7a2c8b2e155ec870fc3
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2022-04-18 00:27:06 +0700

    update script to deploy

--- deploy.bat
+++ deploy.bat
@@ -15,5 +15,9 @@ SET CURRENT_DIR=%~dp0
 SET REACT_BUILD_DIR=%CURRENT_DIR%game-noi-chu\build\
 SET FIREBASE_DIR=%CURRENT_DIR%public\
 call xcopy "%REACT_BUILD_DIR%" "%FIREBASE_DIR%" /Y /E
+::replace url in index.html
+echo please change all / to https://game-noi-chu-nhh.web.app/ before deploy. Then call firebase deploy.
+echo Ctrl+F and replace all ="/ to ="https://game-noi-chu-nhh.web.app/
+::powershell -Command "(gc %FIREBASE_DIR%index.html) -replace '="/', '="https://game-noi-chu-nhh.web.app/' | Out-File -encoding utf8 %FIREBASE_DIR%index2.html"
 ::Put your static files (e.g., HTML, CSS, JS) in your app’s deploy directory (the default is “public”).
-call firebase deploy
+::call firebase deploy
