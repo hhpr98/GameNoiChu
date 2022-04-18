aa4e492dbdac46257c4ac098789e6e52b560336a
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2022-04-18 21:23:04 +0700

    update build script: copy index.html and clean build

--- deploy.bat
+++ deploy.bat
@@ -9,6 +9,11 @@ call firebase login
 ::firebase init
 call firebase init
 
+::replace url in index.html
+SET INDEX_SRC=%CURRENT_DIR%game-noi-chu\src\assets\deploy\index.html
+SET INDEX_DES=%CURRENT_DIR%game-noi-chu\public\index.html
+call xcopy "%INDEX_SRC%" "%INDEX_DES%" /Y
+
 ::build project
 call cd game-noi-chu
 call npm i
@@ -21,10 +26,8 @@ SET REACT_BUILD_DIR=%CURRENT_DIR%game-noi-chu\build\
 SET FIREBASE_DIR=%CURRENT_DIR%public\
 call xcopy "%REACT_BUILD_DIR%" "%FIREBASE_DIR%" /Y /E
 
-::replace url in index.html
-SET INDEX_SRC=%CURRENT_DIR%game-noi-chu\src\assets\deploy\index.html
-SET INDEX_DES=%CURRENT_DIR%public\index.html
-call xcopy "%INDEX_SRC%" "%INDEX_DES%" /Y
-
 ::Put your static files (e.g., HTML, CSS, JS) in your app’s deploy directory (the default is “public”).
 call firebase deploy
+
+:: clean
+@RD /S /Q "%FIREBASE_DIR%"
