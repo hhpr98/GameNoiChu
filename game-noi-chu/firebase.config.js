d6bd90492fd9345890de2e7f88a966b271cceb19
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2022-05-15 21:23:08 +0700

    add firebase into your project

---
+++ game-noi-chu/firebase.config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApIrvY4gekaJymcKPEWWCZYxN5JO3YkU8",
+  authDomain: "game-noi-chu-nhh.firebaseapp.com",
+  databaseURL: "https://game-noi-chu-nhh-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "game-noi-chu-nhh",
+  storageBucket: "game-noi-chu-nhh.appspot.com",
+  messagingSenderId: "37502804667",
+  appId: "1:37502804667:web:ec3d1374d9438aaa76bf9e",
+  measurementId: "G-NFH6X55L6M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// get db
+const db = getFirestore(app)
+
+export {db}
